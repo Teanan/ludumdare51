121c608d14d1825db6e1591a09f1d1cd8625d9bf
Evanaellio <evan.mottais@gmail.com>
2022-10-01 19:10:22 +0200

    Improved lighting

--- Objects/LightPlafonnier.gd
+++ Objects/LightPlafonnier.gd
@@ -1,10 +1,12 @@
 extends Spatial
 
 onready var anim = $AnimationPlayer
+onready var random = RandomNumberGenerator.new()
 
 func _ready():
+	random.randomize()
 	while true:
-		var wait_time = rand_range(2.0, 10.0)
-		var anim_speed = rand_range(0.5, 2)
+		var wait_time = random.randf_range(2.0, 10.0)
+		var anim_speed = random.randf_range(0.5, 1)
 		yield(get_tree().create_timer(wait_time),"timeout")
 		anim.play("Blink", -1, anim_speed)
