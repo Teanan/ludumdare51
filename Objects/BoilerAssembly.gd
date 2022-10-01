2bccbdc9eeebdfda989a45f3c6f0dee88d70d50a
Evanaellio <evan.mottais@gmail.com>
2022-10-01 17:19:39 +0200

    Add better door animation for boiler

--- Objects/BoilerAssembly.gd
+++ Objects/BoilerAssembly.gd
@@ -4,12 +4,20 @@ export (float) var temperature = 25
 export (float) var pressure = 1
 export (float) var coal = 0
 
+onready var anim = $AnimationPlayer
+
 const min_temp = 12
 const max_temp = 125
 const max_pressure = 4
 
 func _ready():
-	pass
+	# For demo purposes only
+	# TODO : open door when coal nugget approaches
+	while true:
+		open_door()
+		yield(get_tree().create_timer(5),"timeout")
+		close_door()
+		yield(get_tree().create_timer(5),"timeout")
 
 func _on_BoilerTick_timeout():
 	if coal > 0:
@@ -21,3 +29,9 @@ func _on_BoilerTick_timeout():
 	pressure = clamp(pressure - 0.05, 0, max_pressure)
 	
 	$Gauge.set_value((temperature - min_temp) * 100 / (max_temp - min_temp))
+
+func open_door():
+	anim.play("open_door")
+
+func close_door():
+	anim.play_backwards("open_door")
