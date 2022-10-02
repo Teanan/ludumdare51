f3d709448b06f983a4158524dbb4128ee31178ca
FoxyHawk <WolfyHawk.Insalan@Gmail.com>
2022-10-02 18:26:45 +0200

    animate extinguisher

--- Objects/Tools/Extinguisher.gd
+++ Objects/Tools/Extinguisher.gd
@@ -1,14 +1,13 @@
 extends GeneralTool
 
-onready var particles = $CPUParticles
-
 func _on_mouse_entered():
 	emit_signal("select", self)
-	self.animate(true)
 
 func _on_mouse_exited():
 	emit_signal("select", null)
-	self.animate(false)
 
 func animate(play: bool):
-	particles.emitting = play
+	if play:
+		$AnimationPlayer.play("extinguishing", -1.0, 1.5)
+	else:
+		$AnimationPlayer.play("RESET")
