6e84203f2e186aeed744f473417c36ebaf653ae9
Tom Spreters <tom.spreters@gmail.com>
2022-10-02 17:07:02 +0200

    bugged gauge

--- Objects/Gauge/GeneralGauge.gd
+++ Objects/Gauge/GeneralGauge.gd
@@ -11,6 +11,8 @@ export (float) var ALLOWED_JITTER_VALUE = 5.0
 
 export (float) var CONVERGENCE_SPEED = 25.0
 
+export (bool) var BUGGED = false
+
 var current_value = 0
 var target_value = 50
 
@@ -31,15 +33,18 @@ func _ready():
 	rotation_node.rotation_degrees.z = MIN_ANGLE_DEG
 
 func _process(delta):
-	var jitterred_target_value = target_value + rand_range(-ALLOWED_JITTER_VALUE, ALLOWED_JITTER_VALUE)
-	if jitterred_target_value < current_value:
-		current_value = current_value - delta * CONVERGENCE_SPEED
+	if BUGGED:
+		rotation_node.rotation_degrees.z -= 1000 * delta
 	else:
-		current_value = current_value + delta * CONVERGENCE_SPEED
+		var jitterred_target_value = target_value + rand_range(-ALLOWED_JITTER_VALUE, ALLOWED_JITTER_VALUE)
+		if jitterred_target_value < current_value:
+			current_value = current_value - delta * CONVERGENCE_SPEED
+		else:
+			current_value = current_value + delta * CONVERGENCE_SPEED
 	
-	#print(current_value)
-	var angle = map(current_value, MIN_VALUE, MAX_VALUE, MIN_ANGLE_DEG, MAX_ANGLE_DEG)
-	#print(angle)
+		#print(current_value)
+		var angle = map(current_value, MIN_VALUE, MAX_VALUE, MIN_ANGLE_DEG, MAX_ANGLE_DEG)
+		#print(angle)
 	
-	rotation_node.rotation_degrees.z = angle
+		rotation_node.rotation_degrees.z = angle
 
