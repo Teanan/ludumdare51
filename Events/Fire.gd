fe120ec1c344215116a751b3f00c6cfba2888d92
Tom Spreters <tom.spreters@gmail.com>
2022-10-03 19:49:51 +0200

    nil check

--- Events/Fire.gd
+++ Events/Fire.gd
@@ -42,7 +42,7 @@ func _on_Leak_mouse_exited():
 	$ActionTimer.stop()
 
 func _on_ActionTimer_timeout():
-	if RoomScene.using_tool and RoomScene.hand.is_in_group(ActionTool):
+	if RoomScene.using_tool and RoomScene.hand != null and RoomScene.hand.is_in_group(ActionTool):
 		if RoomScene.hand.has_method("play_sfx"):
 			RoomScene.hand.play_sfx(true)
 		progress = progress + 3
